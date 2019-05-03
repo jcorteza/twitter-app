0ee89bf098d1984deb336972f99df20ac8a23777
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-05-03 16:06:32 -0500

    initialized Maven project

---
+++ TwitterApp/src/main/java/com/khoros/twitterapp/App.java
@@ -0,0 +1,13 @@
+package com.khoros.twitterapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
