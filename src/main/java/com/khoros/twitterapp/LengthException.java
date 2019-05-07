fba36408d0b654613bb6573342af3ac4dcac51a9
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-05-07 15:01:23 -0500

    added error handling for wrong command and updated lengthException

--- src/main/java/com/khoros/twitterapp/LengthException.java
+++ src/main/java/com/khoros/twitterapp/LengthException.java
@@ -4,10 +4,15 @@ import java.lang.Throwable;
 
 public class LengthException extends Exception {
     private String message = "Your text length: ";
-    private Throwable cause = new Throwable("Tweet text surpassed 280 characters.");
+    private Throwable cause;
 
     public LengthException(int characters) {
         message += characters;
+        if(characters > 280) {
+            cause = new Throwable("Error: Tweet text surpassed 280 characters.");
+        } else if (characters == 0) {
+            cause = new Throwable("Error: No tweet text enetered.");
+        }
     }
 
     public String getExceptionMessage() {
