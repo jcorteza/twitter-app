f28e51a03535a191208e80dcf25b72db181dbe65
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-05-10 16:00:57 -0500

    storing changes

--- src/test/java/com/khoros/twitterapp/HomeFeedResourceTest.java
+++ src/test/java/com/khoros/twitterapp/HomeFeedResourceTest.java
@@ -1,29 +1,7 @@
 package com.khoros.twitterapp;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.ClassRule;
-import org.junit.Test;
-
 import static org.mockito.Mockito.*;
 
 public class HomeFeedResourceTest {
 
-    @ClassRule
-    public static final ResourceTestRule resources = ResourceTestRule.builder().addResource(new HomeFeedResource()).build();
-
-    @Before
-    public void setup() {
-
-    }
-
-    @After
-    public void tearDown() {
-
-    }
-
-    @Test
-    public void testGetPersion() {
-
-    }
 }
