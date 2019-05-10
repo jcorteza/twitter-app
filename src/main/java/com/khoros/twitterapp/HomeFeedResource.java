a444cf569324b130cace35bca7978903bfaf5930
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-05-10 09:59:51 -0500

    updated HomeFeedResource to Respond to client request with data

--- src/main/java/com/khoros/twitterapp/HomeFeedResource.java
+++ src/main/java/com/khoros/twitterapp/HomeFeedResource.java
@@ -4,7 +4,7 @@ import twitter4j.TwitterFactory;
 import twitter4j.Twitter;
 import twitter4j.Status;
 import twitter4j.TwitterException;
-
+import io.dropwizard.jackson.*;
 import java.util.List;
 
 import javax.ws.rs.GET;
@@ -12,23 +12,19 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.GenericEntity;
 
 @Path("/timeline")
-@Produces(MediaType.TEXT_PLAIN)
+@Produces(MediaType.APPLICATION_JSON)
 public class HomeFeedResource {
 
     @GET
     public Response get() {
         Twitter factory = new TwitterFactory().getSingleton();
         try {
-            String lineBreak = "========================================================================";
+            // String lineBreak = "========================================================================";
             List<Status> tweetsFeed = factory.getHomeTimeline();
-            System.out.println("Showing home timeline.");
-            System.out.println(lineBreak);
-            for (Status tweet : tweetsFeed) {
-                System.out.println(tweet.getUser().getName() + "\n" + tweet.getText() + "\n" + lineBreak);
-            }
-            return Response.status(200).build();
+            return Response.status(200).entity(tweetsFeed).type("application/json").build();
         } catch (TwitterException feedException) {
             feedException.printStackTrace();
             System.out.println(feedException.getErrorMessage());
