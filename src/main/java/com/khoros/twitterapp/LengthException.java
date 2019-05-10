a6c8ce920e52e7fba0ad2b1bb88c964d2737b1cc
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-05-10 14:58:52 -0500

    updated pom file with mockito dependency

--- src/main/java/com/khoros/twitterapp/LengthException.java
+++ src/main/java/com/khoros/twitterapp/LengthException.java
@@ -1,25 +1,3 @@
 package com.khoros.twitterapp;
 
-import java.lang.Throwable;
-
-public class LengthException extends Exception {
-    private String message = "Your text length: ";
-    private Throwable cause;
-
-    public LengthException(int characters) {
-        message += characters;
-        if(characters > 280) {
-            cause = new Throwable("Error: Tweet text surpassed 280 characters.");
-        } else if (characters == 0) {
-            cause = new Throwable("Error: No tweet text enetered.");
-        }
-    }
-
-    public String getExceptionMessage() {
-        return message;
-    }
-
-    public String getCauseMessage() {
-        return cause.getMessage();
-    }
-}
+import static org.mockito.Mockito.*;
