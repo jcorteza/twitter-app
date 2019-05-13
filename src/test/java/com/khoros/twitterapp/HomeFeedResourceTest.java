edef3b5165af1c19e0d8c51b447f5925169c16e7
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-05-13 14:58:57 -0500

    updating repo

--- src/test/java/com/khoros/twitterapp/HomeFeedResourceTest.java
+++ src/test/java/com/khoros/twitterapp/HomeFeedResourceTest.java
@@ -6,11 +6,9 @@ import static org.mockito.Mockito.*;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import twitter4j.Twitter;
-import twitter4j.Status;
 import twitter4j.TwitterException;
 
 import javax.ws.rs.core.Response;
-import java.util.List;
 
 public class HomeFeedResourceTest {
 
@@ -20,25 +18,20 @@ public class HomeFeedResourceTest {
     @Mock
     private Twitter mockFactory;
 
-    List<Status> mockStatus;
-    private Response successResponse;
-
     @Before
     public void setup() {
         feedResource = new HomeFeedResource();
         mockFactory = mock(Twitter.class);
 
         try {
-            mockStatus = mockFactory.getHomeTimeline();
+            when(mockFactory.getHomeTimeline()).thenReturn("Twitter API call successful!");
         } catch(TwitterException e) {
             Assert.fail("Test failed due to TwitterException: " + e.getMessage());
         }
-
-        successResponse = Response.status(200).entity(mockStatus).build();
     }
 
     @Test
     public void resourceGetSuccess() {
-        Assert.assertEquals(successResponse, feedResource.get());
+        Assert.assertEquals(Response.status(200).entity("Twitter API call successful!").build(), feedResource.get());
     }
 }
