fa50f71888085511bb07373e5712354375b72954
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-05-14 21:49:20 -0500

    updated configuration class

--- src/main/java/com/khoros/twitterapp/TwitterAppConfiguration.java
+++ src/main/java/com/khoros/twitterapp/TwitterAppConfiguration.java
@@ -1,8 +1,25 @@
 package com.khoros.twitterapp;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
 
-public class TwitterAppConfiguration extends Configuration {
-    private 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
+
+public class TwitterAppConfiguration extends Configuration {
+
+    @Valid
+    @NotNull
+    private OauthFactory oauth = new OauthFactory();
+
+    @JsonProperty
+    public OauthFactory getOauthFactory() {
+        return oauth;
+    }
+
+    @JsonProperty
+    public void setOauthFactory(OauthFactory factory) {
+        this.oauth = factory;
+    }
 }
