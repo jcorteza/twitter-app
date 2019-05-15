761472fe17c98f1fdead1fcae56f9e91c968a741
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-05-15 15:58:32 -0500

    Revert "reverted back"
    This reverts commit 0c9587ebea278e2c9e177eab393c7f57d205e8e7.

--- src/main/java/com/khoros/twitterapp/resources/StatusUpdateResource.java
+++ src/main/java/com/khoros/twitterapp/resources/StatusUpdateResource.java
@@ -15,12 +15,16 @@ import javax.ws.rs.core.Response;
 public class StatusUpdateResource {
 
     private int maxTweetLength;
-    private Configuration conf;
-    private Twitter factory = new TwitterFactory(conf).getInstance();
+    private Twitter factory;
 
     public StatusUpdateResource(Configuration conf, int maxTweetLength) {
-        this.conf = conf;
         this.maxTweetLength = maxTweetLength;
+        this.factory = new TwitterFactory(conf).getInstance();
+    }
+
+    public StatusUpdateResource(Twitter mockFactory) {
+        this.factory = mockFactory;
+        this.maxTweetLength = 280;
     }
 
     @POST
@@ -38,10 +42,5 @@ public class StatusUpdateResource {
         } catch (TwitterException tweetException) {
             return Response.status(tweetException.getStatusCode()).entity("Whoops! Something went wrong. Try again later.").build();
         }
-    }
-
-    // method for testing using mock Twitter object
-    public void setFactory(Twitter factory) {
-        this.factory = factory;
     }
 }
