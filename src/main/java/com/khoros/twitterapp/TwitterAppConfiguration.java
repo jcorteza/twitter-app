4d1951f22a5b67bfa90026142afe4e0a8dafaa86
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-05-15 10:22:24 -0500

    updated config file and .gitignore

--- src/main/java/com/khoros/twitterapp/TwitterAppConfiguration.java
+++ src/main/java/com/khoros/twitterapp/TwitterAppConfiguration.java
@@ -2,6 +2,7 @@ package com.khoros.twitterapp;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
+import jdk.nashorn.internal.runtime.linker.Bootstrap;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
@@ -9,9 +10,21 @@ import javax.validation.constraints.NotNull;
 
 public class TwitterAppConfiguration extends Configuration {
 
+    private Boolean debug;
+
     @Valid
     @NotNull
     private OauthFactory oauth = new OauthFactory();
+
+    @JsonProperty
+    public Boolean getDebug() {
+        return debug;
+    }
+
+    @JsonProperty
+    public void setDebug(Boolean debug) {
+        this.debug = debug;
+    }
 
     @JsonProperty
     public OauthFactory getOauthFactory() {
