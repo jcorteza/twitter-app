7a1c51a118c1aaf7140c6ad023cee7575813ddcd
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-05-16 14:53:01 -0500

    added logs messages to StatusUpdateResource

--- src/main/java/com/khoros/twitterapp/resources/StatusUpdateResource.java
+++ src/main/java/com/khoros/twitterapp/resources/StatusUpdateResource.java
@@ -16,12 +16,15 @@ import javax.ws.rs.core.Response;
 @Produces("application/json")
 public class StatusUpdateResource {
 
+    final Logger logger = LoggerFactory.getLogger(StatusUpdateResource.class);
+
     private int maxTweetLength;
-    private Twitter factory;
+    private Configuration conf;
+    private Twitter factory = new TwitterFactory(conf).getInstance();
 
     public StatusUpdateResource(Configuration conf, int maxTweetLength) {
         this.maxTweetLength = maxTweetLength;
-        this.factory = new TwitterFactory(conf).getInstance();
+        this.conf = conf;
     }
 
     public StatusUpdateResource(Twitter mockFactory) {
@@ -31,17 +34,49 @@ public class StatusUpdateResource {
 
     @POST
     public Response postStatus(@FormParam("message") String tweetText) {
+
         String statusText = tweetText.trim();
+
+        logger.info("Twitter Configuration: {}", conf);
+        logger.info("Status Update Text: {}", statusText);
+
         try {
             if (statusText.length() == 0) {
+
+                logger.info("statusText length: {}", statusText.length());
                 return Response.status(403).entity("No tweet text entered.").build();
+
             } else if (statusText.length() > maxTweetLength) {
+
+                logger.info("statusText length: {}", statusText.length());
                 return Response.status(403).entity("Tweet text surpassed 280 characters.").build();
+
             } else {
+
                 Status newStatus = factory.updateStatus(statusText);
                 return Response.status(200).entity(newStatus).build();
+
             }
         } catch (TwitterException tweetException) {
+
+            logger.info("Twitter Exception thrown." );
+
+            if (tweetException.isCausedByNetworkIssue()) {
+
+                logger.debug("Exception Caused By Network Issues: {}", tweetException.isCausedByNetworkIssue());
+
+            } else if (tweetException.exceededRateLimitation()) {
+
+                logger.debug("Exceed Rate Limitation: {}", tweetException.exceededRateLimitation());
+                logger.debug("Current Rate Limit: {}", tweetException.getRateLimitStatus());
+
+            } else {
+
+                logger.debug("Twitter API Access Level: {}", tweetException.getAccessLevel());
+                logger.debug("TwitterException Error Message: {}", tweetException.getErrorMessage());
+
+            }
+
             return Response.status(tweetException.getStatusCode()).entity("Whoops! Something went wrong. Try again later.").build();
         }
     }
