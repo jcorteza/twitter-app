4faea86c18a6e81f2b7df2bd5585e2f039054d1b
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-06-04 14:20:59 -0500

    removed unnecessary imports

--- src/main/java/com/khoros/twitterapp/TwitterApp.java
+++ src/main/java/com/khoros/twitterapp/TwitterApp.java
@@ -1,15 +1,10 @@
 package com.khoros.twitterapp;
 
 import com.khoros.twitterapp.resources.MainResource;
-import com.khoros.twitterapp.services.CacheUp;
 import com.khoros.twitterapp.services.TwitterService;
 
 import io.dropwizard.Application;
 import io.dropwizard.setup.Environment;
-
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
 
 public class TwitterApp extends Application<TwitterAppConfiguration> {
 
@@ -23,6 +18,6 @@ public class TwitterApp extends Application<TwitterAppConfiguration> {
         TwitterService.getInstance().setTWFactory(configuration.twitter4jConfigurationBuild().build());
 
         environment.jersey().register(new MainResource());
-        
+
     }
 }
