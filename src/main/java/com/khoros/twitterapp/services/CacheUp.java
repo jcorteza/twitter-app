fe5e2e923401c4579eaf507b2b36408e42ccb407
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-06-04 14:20:03 -0500

    removed map and map setters and getters

--- src/main/java/com/khoros/twitterapp/services/CacheUp.java
+++ src/main/java/com/khoros/twitterapp/services/CacheUp.java
@@ -1,20 +1,13 @@
 package com.khoros.twitterapp.services;
 
-import com.khoros.twitterapp.models.CacheStatus;
 import twitter4j.Status;
 
-import java.util.Date;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Map;
 import java.util.Set;
 
 public final class CacheUp {
     // clean up interval 1 week
-    public static final long CLEAN_UP_INTERVAL = 7 * 24 * 60 * 60 * 1000;
-    private HashMap<String, CacheStatus> cacheStatusHashMap = new HashMap<>();
-    private Set<Status> cacheStatusSet = new HashSet<>();
+    private Set<Status> cacheSet = new HashSet<>();
     private static CacheUp instance = new CacheUp();
 
     private CacheUp() {
@@ -25,48 +18,15 @@ public final class CacheUp {
         return instance;
     }
 
-    public HashMap<String, CacheStatus> getCacheStatusHashMap() {
-        return cacheStatusHashMap;
-    }
-
-    public Set<Status> getCacheStatusSet() {
-
-        Date now = new Date();
-        Set<Map.Entry<String, CacheStatus>> cacheStatusEntries = getCacheStatusHashMap().entrySet();
-
-        cacheStatusSet.clear();
-
-        for(Iterator<Map.Entry<String, CacheStatus>> i = cacheStatusEntries.iterator(); i.hasNext();) {
-            Map.Entry<String, CacheStatus> cacheStatusEntry = i.next();
-            long dateCreatedCacheStatus = cacheStatusEntry.getValue().getCacheObjectCreated().getTime();
-
-            if(now.getTime() - dateCreatedCacheStatus >= CLEAN_UP_INTERVAL) {
-
-                i.remove();
-
-            } else {
-
-                cacheStatusSet.add(cacheStatusEntry.getValue().getStatus());
-
-            }
-        }
-
-        return cacheStatusSet;
+    public Set<Status> getCacheSet() {
+        return cacheSet;
     }
 
     public void addStatusToCache(twitter4j.Status status) {
-        String cacheStatusKey = new StringBuilder()
-                .append(status.getCreatedAt())
-                .append("-")
-                .append(status.getUser().getScreenName())
-                .toString();
-        cacheStatusHashMap.putIfAbsent(
-                cacheStatusKey,
-                new CacheStatus(status)
-        );
+        cacheSet.add(status);
     }
 
-    public void setCacheStatusHashMap(HashMap<String, CacheStatus> hashMap) {
-        cacheStatusHashMap = hashMap;
+    public void setCacheSet(Set<Status> cacheSet) {
+        this.cacheSet = cacheSet;
     }
 }
