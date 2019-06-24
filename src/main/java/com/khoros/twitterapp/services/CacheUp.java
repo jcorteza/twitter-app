542d2f10c1cb1e11d00f05e3f4a766464c02a9fd
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-06-24 15:29:22 -0500

    changed cache implementation to HashMap

--- src/main/java/com/khoros/twitterapp/services/CacheUp.java
+++ src/main/java/com/khoros/twitterapp/services/CacheUp.java
@@ -3,29 +3,23 @@ package com.khoros.twitterapp.services;
 import twitter4j.Status;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class CacheUp {
-    private Set<Status> homeTimelineSet = new HashSet<>();
-    private Set<Status> userTimelineSet = new HashSet<>();
+//    private Set<Status> homeTimelineSet = new HashSet<>();
+//    private Set<Status> userTimelineSet = new HashSet<>();
     private HashMap<String, Set<Status>> cacheMap = new HashMap<>();
 
-    public Set<Status> getHomeTimelineSet() {
-        return homeTimelineSet;
+    public Set<Status> getTimelineSet(String setType) {
+        return cacheMap.get(setType);
     }
 
-    public Set<Status> getUserTimelineSet() {
-        return userTimelineSet;
-    }
+    public void addStatusToCache(String setType, List<Status> statusList) {
+        Set<Status> currentSet = cacheMap.get(setType);
 
-    public void addToHomeTimelineSet(List<Status> statusList) {
-        statusList.forEach(status -> homeTimelineSet.add(status));
-    }
-
-    public void addToUserTimelineSet(List<Status> statusList) {
-        statusList.forEach(status -> userTimelineSet.add(status));
+        statusList.forEach(status -> currentSet.add(status));
+        cacheMap.replace(setType,currentSet);
     }
 
 }
