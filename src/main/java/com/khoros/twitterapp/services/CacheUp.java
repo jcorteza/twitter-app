4c16584d67ef4b0dcd495afd49de05b9c5ed8b92
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-06-24 14:51:49 -0500

    removed unnecessary comment

--- src/main/java/com/khoros/twitterapp/services/CacheUp.java
+++ src/main/java/com/khoros/twitterapp/services/CacheUp.java
@@ -2,14 +2,15 @@ package com.khoros.twitterapp.services;
 
 import twitter4j.Status;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class CacheUp {
-    // clean up interval 1 week
     private Set<Status> homeTimelineSet = new HashSet<>();
     private Set<Status> userTimelineSet = new HashSet<>();
+    private HashMap<String, Set<Status>> cacheMap = new HashMap<>();
 
     public Set<Status> getHomeTimelineSet() {
         return homeTimelineSet;
