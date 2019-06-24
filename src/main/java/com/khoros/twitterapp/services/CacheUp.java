2883ba325156a0e47f02089f90d4f0aeeaa6bfb8
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-06-24 16:10:14 -0500

    moved cachup initialization to constructor

--- src/main/java/com/khoros/twitterapp/services/CacheUp.java
+++ src/main/java/com/khoros/twitterapp/services/CacheUp.java
@@ -8,7 +8,13 @@ import java.util.List;
 import java.util.Set;
 
 public class CacheUp {
+    private Set<Status> initialSet = new HashSet<>();
     private HashMap<TwitterService.CacheSetType, Set<Status>> cacheMap = new HashMap<>();
+
+    public CacheUp() {
+        cacheMap.put(TwitterService.CacheSetType.HOME, initialSet);
+        cacheMap.put(TwitterService.CacheSetType.USER, initialSet);
+    }
 
     public Set<Status> getTimelineSet(TwitterService.CacheSetType setType) {
         return cacheMap.get(setType);
@@ -16,22 +22,10 @@ public class CacheUp {
 
     public void addStatusToCache(TwitterService.CacheSetType setType, List<Status> statusList) {
 
-        Set<Status> currentSet;
+        Set<Status> currentSet = cacheMap.get(setType);
 
-        if(cacheMap.containsKey(setType)) {
-
-            currentSet = cacheMap.get(setType);
-
-            statusList.forEach(status -> currentSet.add(status));
-            cacheMap.replace(setType,currentSet);
-
-        } else {
-
-            currentSet = new HashSet<>();
-
-            statusList.forEach(status -> currentSet.add(status));
-            cacheMap.put(setType, currentSet);
-        }
+        statusList.forEach(status -> currentSet.add(status));
+        cacheMap.replace(setType,currentSet);
 
     }
 
