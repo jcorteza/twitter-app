565451209af90d686405d53cbe739030fd3adb73
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-06-25 17:55:33 -0500

    removed unnecessary lines and replaced set with list

--- src/main/java/com/khoros/twitterapp/services/CacheUp.java
+++ src/main/java/com/khoros/twitterapp/services/CacheUp.java
@@ -3,29 +3,18 @@ package com.khoros.twitterapp.services;
 import twitter4j.Status;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class CacheUp {
-    private HashMap<TwitterService.CacheSetType, Set<Status>> cacheMap = new HashMap<>();
+    private HashMap<TwitterService.CacheSetType, List<Status>> cacheMap = new HashMap<>();
 
-    public CacheUp() {
-        cacheMap.put(TwitterService.CacheSetType.HOME, new HashSet<>());
-        cacheMap.put(TwitterService.CacheSetType.USER, new HashSet<>());
-    }
-
-    public Set<Status> getTimelineCache(TwitterService.CacheSetType setType) {
-        System.out.println(setType);
+    public List<Status> getTimelineCache(TwitterService.CacheSetType setType) {
         return cacheMap.get(setType);
     }
 
     public void addStatusToCache(TwitterService.CacheSetType setType, List<Status> statusList) {
 
-        Set<Status> currentSet = cacheMap.get(setType);
-
-        currentSet.addAll(statusList);
-        cacheMap.replace(setType, currentSet);
+        cacheMap.put(setType, statusList);
 
     }
 
