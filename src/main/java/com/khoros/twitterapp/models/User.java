675fa565360377c86819051b5abe0c167552776e
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-07-08 11:48:50 -0500

    removed userID field

--- src/main/java/com/khoros/twitterapp/models/User.java
+++ src/main/java/com/khoros/twitterapp/models/User.java
@@ -2,25 +2,11 @@ package com.khoros.twitterapp.models;
 
 public class User {
 
-    private long userID;
-
     private String twHandle;
 
     private String name;
 
     private String profileImageUrl;
-
-    public void setUserID(long id) {
-
-        this.userID = id;
-
-    }
-
-    public long getUserID() {
-
-        return userID;
-
-    }
 
     public void setTwHandle(String twHandle) {
 
