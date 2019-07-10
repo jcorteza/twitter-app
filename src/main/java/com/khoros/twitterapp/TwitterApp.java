b23c48a6400835a7e84aabd35268125929f00670
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-07-10 14:17:21 -0500

    used FilterRegistration.Dynamic to set CrossOriginFilter

--- src/main/java/com/khoros/twitterapp/TwitterApp.java
+++ src/main/java/com/khoros/twitterapp/TwitterApp.java
@@ -2,6 +2,11 @@ package com.khoros.twitterapp;
 
 import io.dropwizard.Application;
 import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
 
 public class TwitterApp extends Application<TwitterAppConfiguration> {
 
@@ -11,6 +16,10 @@ public class TwitterApp extends Application<TwitterAppConfiguration> {
 
     @Override
     public void run(TwitterAppConfiguration configuration, Environment environment) {
+
+        final FilterRegistration.Dynamic cors = environment.servlets().addFilter("cors", CrossOriginFilter.class);
+        cors.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, "*");
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
 
         TwitterFactoryComponent twComponent = DaggerTwitterFactoryComponent.builder()
                 .twitterFactoryModule(
@@ -22,5 +31,6 @@ public class TwitterApp extends Application<TwitterAppConfiguration> {
                 .build();
 
         environment.jersey().register(resourceComponent.mainResource());
+
     }
 }
