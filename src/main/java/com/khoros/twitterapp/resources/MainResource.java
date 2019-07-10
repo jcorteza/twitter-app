8c81e2d64fc5f061bd90d3c50549d92cc891d4dc
Josephine Cortez-Ayala <josephine.cortez-ayala@khoros.com>
2019-07-10 13:59:48 -0500

    Revert "httpservletresponse.setHeader"
    This reverts commit dff405c4b683c715f74c9e04ae1b280fbb59bd12.

--- src/main/java/com/khoros/twitterapp/resources/MainResource.java
+++ src/main/java/com/khoros/twitterapp/resources/MainResource.java
@@ -7,7 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
-import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Path;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
@@ -15,7 +14,6 @@ import javax.ws.rs.POST;
 import javax.ws.rs.GET;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -23,17 +21,15 @@ import javax.ws.rs.core.Response;
 @Produces(MediaType.APPLICATION_JSON)
 public class MainResource {
 
-    @Context
-    private HttpServletResponse servletResponse;
     private final Logger logger = LoggerFactory.getLogger(MainResource.class);
     private final TwitterService twitterService;
     private final String headerACAO = "Access-Control-Allow-Origin";
     private final String origin = "*";
+    private Response baseResponse = Response.noContent().header(headerACAO, origin).build();
 
     @Inject
     public MainResource(TwitterService twitterService) {
         this.twitterService = twitterService;
-        servletResponse.setHeader(headerACAO, origin);
     }
 
     @Path("/tweet")
@@ -49,10 +45,12 @@ public class MainResource {
 
             return twitterService.updateStatus(statusText)
                     .map(newStatus -> Response
+                            .fromResponse(baseResponse)
                             .status(Response.Status.CREATED)
                             .entity(newStatus)
                             .build())
                     .orElse(Response
+                            .fromResponse(baseResponse)
                             .status(Response.Status.INTERNAL_SERVER_ERROR)
                             .entity(TwitterService.GENERAL_ERR_MSG)
                             .build());
@@ -76,10 +74,12 @@ public class MainResource {
 
             return twitterService.getHomeTimeline()
                     .map(feedResponse -> Response
+                            .fromResponse(baseResponse)
                             .status(Response.Status.OK)
                             .entity(feedResponse)
                             .build())
                     .orElse(Response
+                            .fromResponse(baseResponse)
                             .status(Response.Status.INTERNAL_SERVER_ERROR)
                             .entity(TwitterService.GENERAL_ERR_MSG)
                             .build());
@@ -105,10 +105,12 @@ public class MainResource {
 
             return twitterService.getFilteredHomeTimeline(keyword)
                     .map(feedResponse -> Response
+                            .fromResponse(baseResponse)
                             .status(Response.Status.OK)
                             .entity(feedResponse)
                             .build())
                     .orElse(Response
+                            .fromResponse(baseResponse)
                             .status(Response.Status.INTERNAL_SERVER_ERROR)
                             .entity(TwitterService.GENERAL_ERR_MSG)
                             .build());
@@ -133,10 +135,12 @@ public class MainResource {
 
             return twitterService.getUserTimeline()
                     .map(feedResponse -> Response
+                            .fromResponse(baseResponse)
                             .status(Response.Status.OK)
                             .entity(feedResponse)
                             .build())
                     .orElse(Response
+                            .fromResponse(baseResponse)
                             .status(Response.Status.INTERNAL_SERVER_ERROR)
                             .entity(TwitterService.GENERAL_ERR_MSG)
                             .build());
@@ -161,10 +165,12 @@ public class MainResource {
 
             return twitterService.replyToTweet(statusText.trim(), inReplyToID)
                     .map(newStatus -> Response
+                        .fromResponse(baseResponse)
                         .status(Response.Status.CREATED)
                         .entity(newStatus)
                         .build())
                     .orElse(Response
+                        .fromResponse(baseResponse)
                         .status(Response.Status.INTERNAL_SERVER_ERROR)
                         .entity(TwitterService.GENERAL_ERR_MSG)
                         .build());
